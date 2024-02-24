00cca07597180d11633d4875c9422b1e573341fe
R3D1NM <r3dinm@gmail.com>
2024-02-24 22:16:27 +0900

    [style] MainScreen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,19 +1,50 @@
 import React from 'react'
-import { SafeAreaView, StatusBar, Text, View } from 'react-native'
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 
 const MainScreen = () => {
     return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
         <StatusBar barStyle={'default'} />
-        <Text>ToDo App</Text>
-        <View>
-            <Text>To Do</Text>
+        <Text style={styles.pageTitle}>ToDo App</Text>
+        <View style={styles.listView}>
+            <Text style={styles.listTitle}>To Do</Text>
         </View>
-        <View>
-            <Text>Done</Text>
+        <View style={styles.separator} />
+        <View style={styles.listView}>
+            <Text style={styles.listTitle}>Done</Text>
         </View>
     </SafeAreaView>
     )
 }
 
 export default MainScreen
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 20 :0,
+        backgroundColor: '#f7f8fa'
+    },
+    pageTitle:{
+        marginBottom: 35,
+        paddingHorizontal: 15,
+        fontSize: 54,
+        fontWeight: '600'
+    },
+    listView:{
+        flex: 1
+
+    },
+    listTitle:{
+        marginBottom: 25,
+        paddingHorizontal: 15,
+        fontSize: 41,
+        fontWeight: "500"
+    },
+    separator:{
+        marginHorizontal: 10,
+        marginTop: 25,
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(0,0,0,0.2)'
+    }
+})
